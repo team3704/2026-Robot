109a7efe47f740538ed32501c45269c1d410b12f
team3704 <suhiteam3704@aol.com>
2026-01-22 17:45:54 -0800

    Update ExampleSubsystem.java

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public ExampleSubsystem() {}
-
+// we live in a simulation created by a secret company called life
   /**
    * Example command factory method.
    *
