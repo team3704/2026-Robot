187a2657491a3dfb7aaab3eeb4c57e24215f9997
team3704 <suhiteam3704@aol.com>
2026-02-26 16:05:29 -0800

    pid yay

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -2,7 +2,7 @@ package frc.robot.commands;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-import edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
@@ -11,20 +11,10 @@ import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 public class Shoot extends Command {
 
     private Shooter m_shoot;
-    private TorqueCurrentFOC torqueveldude = new TorqueCurrentFOC(0);
 
     public Shoot(Shooter subsystem) {
         m_shoot = subsystem;
         addRequirements(subsystem);
-
-        var configs = new Slot0Configs();
-        configs.kS = 2.5; // To account for friction, add 2.5 A of static feedforward
-        configs.kP = 5; // An error of 1 rotation per second results in 5 A output
-        configs.kI = 0; // No output for integrated error
-        configs.kD = 0; // No output for error derivative
-        // Peak output of 40 A
-        configs.withPeakForwardTorqueCurrent(Amps.of(40))
-            .withPeakReverseTorqueCurrent(Amps.of(-40));
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,38 +6,44 @@ import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
 
 public class Shooter extends SubsystemBase {
 
-    private final TalonFX leftShooterLeader;
-    private final TalonFX rightShooterFollower;
+    private final TalonFX leftShooter;
+    private final TalonFX rightShooter;
 
     private TorqueCurrentFOC torqueveldude = new TorqueCurrentFOC(0);
+    private PIDController Lpiddy = new PIDController(0, 0, 0);
+    private PIDController Rpiddy = new PIDController(0, 0, 0);
 
     public double Speed = 6.0;
 
     public Shooter() {
 
-        leftShooterLeader = new TalonFX(0);
-        rightShooterFollower = new TalonFX(1);
-        
-        rightShooterFollower.setControl(new Follower(0, MotorAlignmentValue.Aligned));
-        
-        var configs = new Slot0Configs();
-        configs.kS = 2.5; // To account for friction, add 2.5 A of static feedforward
-        configs.kP = 5; // An error of 1 rotation per second results in 5 A output
-        configs.kI = 0; // No output for integrated error
-        configs.kD = 0; // No output for error derivative
+        leftShooter = new TalonFX(0);
+        rightShooter = new TalonFX(1);
 
-        leftShooterLeader.getConfigurator().apply(configs);
+        // var configs = new Slot0Configs();
+        // configs.kS = 2.5; // To account for friction, add 2.5 A of static feedforward
+        // configs.kP = 5; // An error of 1 rotation per second results in 5 A output
+        // configs.kI = 0; // No output for integrated error
+        // configs.kD = 0; // No output for error derivative
+
+        // leftShooterLeader.getConfigurator().apply(configs);
     }
 
     public void Start() {
-        leftShooterLeader.setControl(torqueveldude.withOutput(Speed));
+        // leftShooterLeader.setControl(torqueveldude.withOutput(Speed));
+        leftShooter.set(Lpiddy.calculate(Speed));
+        rightShooter.set(Rpiddy.calculate(Speed));
+
     }
 
     public void Stop() {
-        leftShooterLeader.stopMotor();
+        leftShooter.stopMotor();
+        rightShooter.stopMotor();
     }
 }
