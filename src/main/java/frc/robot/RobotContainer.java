30b4f5424b894191c3a5ebfa9024736cc92afe70
team3704 <suhiteam3704@aol.com>
2026-02-26 16:05:59 -0800

    oops

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,6 @@
 
 package frc.robot;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.AdjustDistance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -32,7 +30,7 @@ public class RobotContainer {
 
   private void configureBindings() 
   {
-    
+
   }
 
   /**
